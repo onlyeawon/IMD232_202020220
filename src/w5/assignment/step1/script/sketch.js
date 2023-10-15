a602708a196ded4d8920d9c5e884cd41239316ad
onlyeawon <126849525+onlyeawon@users.noreply.github.com>
2023-10-16 05:48:44 +0900

    머리야머리ㅑㅇ

--- src/w5/assignment/step1/script/sketch.js
+++ src/w5/assignment/step1/script/sketch.js
@@ -1,41 +1,44 @@
-const cNum = 8;
-const rNum = 8;
-let gridC;
-let gridR;
+const stripeNum = 8;
+const stripeNum2 = 8;
+const gridC = 10; // 열의 수
+const gridR = 10; // 행의 수
+const stripeBegin = 60;
+const stripeGap = 60;
+
 let angleBegin = 0;
-let angleBeginVel;
-let angleStep;
+let angleBeginVel = 0.0001; // 각도 변화 속도 (15도씩 증가)
+let angleStep = PI / 15; // 선을 그릴 때 한 번에 증가할 각도 (15도)
 
 function setup() {
   setCanvasContainer('canvas', 1, 1, true);
-
-  colorMode(HSL, 360, 100, 100, 100);
-  background(360, 0, 100);
+  background(255);
 }
 
 function draw() {
-  background(360, 0, 100);
+  background(255);
 
-  for (let r = 0; r < rNum; r++) {
-    for (let c = 0; c < cNum; c++) {
-      push();
-      translate();
-      rotate();
-      pop();
-    }
-  }
-
-  noStroke();
   rectMode(CENTER);
-  for (let a = 0; a < cNum; a++) {
-    for (let b = 0; b < rNum; b++) {
-      fill((255 / cNum) * a, (255 / rNum) * b, 255);
-      let x = ((a + 1) * width) / (cNum + 1);
-      let y = ((b + 1) * height) / (rNum + 1);
 
-      ellipse(x, y, 10);
+  let marginX = stripeBegin; // 좌우 마진 설정
+  let marginY = stripeBegin; // 상하 마진 설정
+  let gapX = (width - 2 * marginX) / (gridC - 1); // 가로 간격 계산
+  let gapY = (height - 2 * marginY) / (gridR - 1); // 세로 간격 계산
+
+  for (let b = 0; b < gridR; b++) {
+    for (let a = 0; a < gridC; a++) {
+      push(); // 현재의 그림 상태 저장
+
+      translate(marginX + a * gapX, marginY + b * gapY); // 중심으로 이동
+      rotate(angleBegin); // 각도 조절
+      stroke(a * (360 / gridC), b * (100 / gridR), 100);
+
+      ellipse(0, 0, 60);
+
+      line(0, 0, 40, 0); // 중심에서 선 그리기
+
+      pop(); // 그림 상태 복원
+
+      angleBegin += angleBeginVel; // 각도 증가
     }
   }
-
-  angleBegin += angleBeginVel;
 }
---
+++ src/w5/assignment/step2/script/sketch.js
@@ -0,0 +1,31 @@
+let pend;
+let gravity;
+
+function setup() {
+  setCanvasContainer('canvas', 1, 1, true);
+  background(255);
+
+  pend = new Pendulum(width / 2, 10, height / 2, (TAU / 360) * 30, 50);
+  gravity = createVector(0, 1);
+}
+
+function draw() {
+  pend.applyForce(gravity);
+  pend.update();
+
+  background(255);
+  pend.display();
+}
+
+function mouseMoved() {
+  pend.mouseMoved(mouseX, mouseY);
+}
+function mousePressed() {
+  pend.mousePressed(mouseX, mouseY);
+}
+function mouseDragged() {
+  pend.mouseDragged(mouseX, mouseY);
+}
+function mouseReleased() {
+  pend.mouseReleased();
+}
